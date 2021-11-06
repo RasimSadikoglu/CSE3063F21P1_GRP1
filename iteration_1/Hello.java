fe695a8635553a027366f30f1dc3866cb6f52ccc
nazmigorkem <69164892+nazmigorkem@users.noreply.github.com>
2021-11-06 18:31:30 +0300

    removed gson tests and TC

--- iteration_1/Hello.java
+++ iteration_1/Hello.java
@@ -6,11 +6,5 @@ import Util.DataHandler;
 class Hello {
     public static void main (String[] args) {
         System.out.println("Hello World");
-        Course cs = new Course("b", "c", 2);
-        ArrayList<Course> a = new ArrayList<>();
-        a.add(cs);
-        DataHandler<ArrayList<Course>> dh = new DataHandler<>();
-        dh.writeToJson(a, ".", "Courses.json"); 
-        System.out.println(dh.parseFromJson(".", "Courses.json").get(0));
     }
 }
--- iteration_1/People/Teacher.java
+++ iteration_1/People/Teacher.java
@@ -10,7 +10,7 @@ public class Teacher extends Person{
     private int[] officeHours; // [startHour, startMinute, endHour, endMinute]
     private String officeRoom;
 
-    public Teacher(String name, String mail, String phoneNumber, String gender, int age, String TC, String department, String officeRoom) {
+    public Teacher(String name, String mail, String phoneNumber, String gender, int age, String department, String officeRoom) {
         super(name, mail, phoneNumber, gender, age, department);
         this.officeRoom = officeRoom;
     }
