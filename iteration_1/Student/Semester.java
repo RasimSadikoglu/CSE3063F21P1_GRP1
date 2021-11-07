ffff3c7f68554d5185d89fef9cd6aaeecb2bc16b
burakcglyn <cglynburak@outlook.com>
2021-11-07 18:31:55 +0300

    creating map for holding courses and notes

--- iteration_1/Student/Semester.java
+++ iteration_1/Student/Semester.java
@@ -1,5 +1,24 @@
 package Student;
+import java.util.*;
+import Course.Course;
 
 public class Semester {
-    
+
+    private Map<Course, Float> notes=new TreeMap<Course, Float>();
+
+    public Semester() {
+        notes = new TreeMap<Course, Float>();
+    }
+
+    public void addNote(Course course, float note){
+        notes.put(course,note);  
+    }
+
+    public void addNewCourse(Course course){
+        notes.put(course,(float) -2);
+    }
+
+    public Map<Course, Float> getNotes() {
+        return notes;
+    }
 }
