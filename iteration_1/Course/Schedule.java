b0a2f78314c512ef6a176f90fcf0b8f17f902e8c
RasimSadikoglu <sadikoglurasim@gmail.com>
2021-11-07 17:26:16 +0300

    schedule class is completed & transcript class is completed except for the gpa calculation

--- iteration_1/Course/Schedule.java
+++ iteration_1/Course/Schedule.java
@@ -1,5 +1,33 @@
 package Course;
 
+import java.util.ArrayList;
+
 public class Schedule {
     
+    // Time Format --> "$DAY-$STARTHOUR.$STARTMINUTE-$ENDHOUR.$ENDMINUTE" (0 for Monday, 6 for Sunday)
+    private ArrayList<String> timeTable;
+
+    public Schedule() {
+        timeTable = new ArrayList<String>();
+    }
+
+    public Schedule(ArrayList<String> timeTable) {
+        this.timeTable = timeTable;
+    }
+
+    public void addTime(String time) {
+        timeTable.add(time);
+    }
+
+    // public void deleteTime(String time) {
+    //     timeTable.remove(time);
+    // }
+
+    // public void deleteTime(int index) {
+    //     timeTable.remove(index);
+    // }
+
+    public ArrayList<String> getTimeTable() {
+        return timeTable;
+    } 
 }
--- iteration_1/Student/Transcript.java
+++ iteration_1/Student/Transcript.java
@@ -1,5 +1,44 @@
 package Student;
 
+import java.util.ArrayList;
+
 public class Transcript {
     
+    private ArrayList<Semester> semesters;
+
+    public Transcript() {
+        semesters = new ArrayList<Semester>();
+    }
+
+    public Transcript(ArrayList<Semester> semesters) {
+        this.semesters = semesters;
+    }
+
+    public void addSemester(Semester s) {
+        semesters.add(s);
+    }
+
+    public Semester getCurrentSemester() {
+        if (semesters.size() == 0) {
+            throw new IndexOutOfBoundsException("Failed to obtain current semester information! (Semester array is empty!)");
+        }
+
+        return semesters.get(semesters.size() - 1);
+    }
+
+    public float getGPA() {
+        return getCompletedCredits() / getTotalCourseCredits();
+    }
+
+    public float getTotalCourseCredits() { // Waiting for semester class to be completed
+        return 1;
+    }
+
+    public float getCompletedCredits() { // Waiting for semester class to be completed
+        return 1;
+    }
+
+    public ArrayList<Semester> getSemesters() {
+        return semesters;
+    }
 }
