7c14ed2be1e0d158fd003c75a803db024063f5f6
nazmigorkem <69164892+nazmigorkem@users.noreply.github.com>
2021-11-07 15:27:29 +0300

    business model changes

--- iteration_1/Hello.java
+++ iteration_1/Hello.java
@@ -1,8 +1,3 @@
-import java.util.ArrayList;
-
-import Courses.Course;
-import Util.DataHandler;
-
 class Hello {
     public static void main (String[] args) {
         System.out.println("Hello World");
