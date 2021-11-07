f098c1634b845e1e35a26c81ccf9064a4b4342a1
nazmigorkem <69164892+nazmigorkem@users.noreply.github.com>
2021-11-07 18:06:36 +0300

    added getter setter for fields

--- iteration_1/Student/Student.java
+++ iteration_1/Student/Student.java
@@ -7,4 +7,20 @@ public class Student {
         this.id = id;
         this.transcript = transcript;
     }
+
+    public String getId() {
+        return id;
+    }
+
+    public Transcript getTranscript() {
+        return transcript;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setTranscript(Transcript transcript) {
+        this.transcript = transcript;
+    }
 }
