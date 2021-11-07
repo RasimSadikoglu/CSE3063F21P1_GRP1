2eaf4a1d4151b38e8555b360b3891eafd2d4020b
nazmigorkem <69164892+nazmigorkem@users.noreply.github.com>
2021-11-07 18:04:50 +0300

    added student constructor

--- iteration_1/Student/Student.java
+++ iteration_1/Student/Student.java
@@ -1,5 +1,10 @@
 package Student;
 
 public class Student {
-
+    private String id;
+    private Transcript transcript;
+    public Student(String id, Transcript transcript) {
+        this.id = id;
+        this.transcript = transcript;
+    }
 }
