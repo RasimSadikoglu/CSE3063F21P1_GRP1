4d831b970754a967c6ada59a164a744d6cf5e025
Erkam Karaca <42948198+rkmkrc@users.noreply.github.com>
2021-11-08 10:17:59 +0300

    Add files via upload
    Course class added.

---
+++ Course.java
@@ -0,0 +1,79 @@
+import Schedule.Schedule;
+
+package Course;
+public class Course {
+
+    private String courseName;
+    private String courseCode;
+    private float requiredCredits;
+    private String prerequisiteCourse;
+    private float courseCredits;
+    private int courseQuota;
+    private Schedule courseSchedule;
+
+    public Course(String courseName, String courseCode,float requiredCredits, String prerequisiteCourse,float courseCredits, int courseQuota, Schedule courseSchedule){
+        this.courseName = courseName;
+        this.courseCode = courseCode;
+        this.requiredCredits = requiredCredits;
+        this.prerequisiteCourse = prerequisiteCourse;
+        this.courseCredits = courseCredits;
+        this.courseQuota = courseQuota;
+        this.courseSchedule = courseSchedule;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
+    }
+
+    public float getRequiredCredits() {
+        return requiredCredits;
+    }
+
+    public void setRequiredCredits(float requiredCredits) {
+        this.requiredCredits = requiredCredits;
+    }
+
+    public String getPrerequisiteCourse() {
+        return prerequisiteCourse;
+    }
+
+    public void setPrerequisiteCourse(String prerequisiteCourse) {
+        this.prerequisiteCourse = prerequisiteCourse;
+    }
+
+    public float getCourseCredits() {
+        return courseCredits;
+    }
+
+    public void setCourseCredits(float courseCredits) {
+        this.courseCredits = courseCredits;
+    }
+
+    public int getCourseQuota() {
+        return courseQuota;
+    }
+
+    public void setCourseQuota(int courseQuota) {
+        this.courseQuota = courseQuota;
+    }
+
+    public Schedule getCourseSchedule() {
+        return courseSchedule;
+    }
+
+    public void setCourseSchedule(Schedule courseSchedule) {
+        this.courseSchedule = courseSchedule;
+    }
+}
